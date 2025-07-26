2b83c60586c9025c2d521580e75d52e4ad3237d1
Malloway00 <95506632+Malloway00@users.noreply.github.com>
2025-07-26 13:20:53 -0400

    Implement basic shop NPC

--- gamemode/entities/loner_rp_npc/shared.lua
+++ gamemode/entities/loner_rp_npc/shared.lua
@@ -1,4 +1,10 @@
 ENT.Type = "ai"
 ENT.Base = "base_ai"
 ENT.PrintName = "Shopkeeper"
+ENT.Category = "LonerRP"
 ENT.Spawnable = true
+ENT.AutomaticFrameAdvance = true
+
+function ENT:SetAutomaticFrameAdvance(bUsingAnim)
+    self.AutomaticFrameAdvance = bUsingAnim
+end
