84629a5716753110f70d70702f3ad4d464b2a240
Malloway00 <95506632+Malloway00@users.noreply.github.com>
2025-07-26 13:17:28 -0400

    Add basic jobs and persistence

--- gamemode/entities/loner_rp_npc/init.lua
+++ gamemode/entities/loner_rp_npc/init.lua
@@ -1,1 +1,18 @@
--- Serverside NPC code placeholder
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+function ENT:Initialize()
+    self:SetModel("models/Humans/Group01/female_01.mdl")
+    self:SetHullType(HULL_HUMAN)
+    self:SetUseType(SIMPLE_USE)
+    self:PhysicsInit(SOLID_BBOX)
+    self:SetSolid(SOLID_BBOX)
+end
+
+function ENT:AcceptInput(name, activator)
+    if name == "Use" and IsValid(activator) and activator:IsPlayer() then
+        net.Start("open_npc_menu")
+        net.Send(activator)
+    end
+end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -4,3 +4,38 @@ GM.Email = "N/A"
 GM.Website = "N/A"
 
 DeriveGamemode("sandbox")
+
+-- Job table defining the available jobs and their loadouts
+g_JOBS = {
+    citizen = {
+        name = "Citizen",
+        model = "models/player/Group01/male_07.mdl",
+        weapons = {}
+    },
+
+    police = {
+        name = "Police Officer",
+        model = "models/player/police.mdl",
+        weapons = {"weapon_pistol"}
+    }
+}
+
+local PLAYER = FindMetaTable("Player")
+
+function PLAYER:SetMoney(amount)
+    self:SetNWInt("cash", amount or 0)
+end
+
+function PLAYER:AddMoney(amount)
+    self:SetMoney(self:GetNWInt("cash", 0) + (amount or 0))
+end
+
+function PLAYER:GetMoney()
+    return self:GetNWInt("cash", 0)
+end
+
+function PLAYER:SetJob(id)
+    if not g_JOBS[id] then return end
+    self:SetNWString("job", id)
+end
+
